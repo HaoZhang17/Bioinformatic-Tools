9253fc17282c3cc631b039e880c7fd1b74eac2d8
Hao Zhang <30916517+HaoZhang17@users.noreply.github.com>
2019-10-16 22:22:41 +0800

    Update extractJunctionSite.py

--- extractJunctionSite.py
+++ extractJunctionSite.py
@@ -1,7 +1,6 @@
 #! usr/bin/env python3
 
-import re
-import argparse
+import re, argparse
 
 parser = argparse.ArgumentParser(
     description = '''This script is used for extract junction from gtf files.''',
